ce267b841640ad5a570182ab880c1a9ef4d1b9a0
bkdragon0228 <rlaqjarb2067@naver.com>
2022-07-06 17:18:43 +0900

    기능 : 체크박스 업데이트

--- voca/src/component/Word.js
+++ voca/src/component/Word.js
@@ -10,7 +10,20 @@ const Word = ({ word }) => {
     };
 
     const toggleDone = () => {
-        setIsDone(!isDone);
+        fetch(`http://localhost:3001/words/${word.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                ...word,
+                isDone: !isDone,
+            }),
+        }).then((res) => {
+            if (res.ok) {
+                setIsDone(!isDone);
+            }
+        });
     };
     return (
         <tr className={isDone ? 'off' : ''}>
