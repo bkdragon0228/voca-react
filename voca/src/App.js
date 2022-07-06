dd9d90379fff095f7ff7d287f87c851d8d6e841e
bkdragon0228 <rlaqjarb2067@naver.com>
2022-07-06 16:10:01 +0900

    기능 : 잘못된 접근 처리하기

--- voca/src/App.js
+++ voca/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Day from './component/Day';
 import DayList from './component/DayList';
 import Header from './component/Header';
+import Empty from './component/Empty';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -11,6 +13,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<DayList />} />
                 <Route path="/day/:day" element={<Day />} />
+                <Route path="*" element={<Empty />} />
             </Routes>
         </Router>
     );
