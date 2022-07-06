f134e40f083f4c2a09e236be97301b887f6e8e6b
bkdragon0228 <rlaqjarb2067@naver.com>
2022-07-06 16:21:07 +0900

    기능 : 단어 뜻 보기 버튼 구현
    state를 이용해 단어의 뜻을 보고 숨기고하는 버튼의 기능을 구현했다.

--- voca/src/component/Word.js
+++ voca/src/component/Word.js
@@ -1,13 +1,23 @@
+import { useState } from 'react';
+
 const Word = ({ word }) => {
+    const [isShow, setIsShow] = useState(false);
+
+    const handleClick = () => {
+        // setIsShow((prev) => (prev ? false : true));
+        setIsShow((prev) => !prev);
+    };
     return (
         <tr>
             <td>
                 <input type="checkbox"></input>
             </td>
             <td>{word.eng}</td>
-            <td>{word.kor}</td>
+            <td>{isShow && word.kor}</td>
             <td>
-                <button>뜻 보기</button>
+                <button onClick={handleClick}>
+                    뜻 {isShow ? '숨기기' : '보기'}
+                </button>
                 <button class="btn_del">삭제</button>
             </td>
         </tr>
