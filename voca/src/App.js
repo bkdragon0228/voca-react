f7aa43e149fe3fb1272aeb8b2b85a1349a5dd02b
bkdragon0228 <rlaqjarb2067@naver.com>
2022-07-07 00:01:39 +0900

    기능 : day 추가

--- voca/src/App.js
+++ voca/src/App.js
@@ -6,6 +6,7 @@ import Empty from './component/Empty';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CreateWord from './component/CreateWord';
+import CreateDay from './component/CreateDay';
 
 function App() {
     return (
@@ -15,6 +16,7 @@ function App() {
                 <Route path="/" element={<DayList />} />
                 <Route path="/day/:day" element={<Day />} />
                 <Route path="/create_word" element={<CreateWord />} />
+                <Route path="/create_day" element={<CreateDay />} />
                 <Route path="*" element={<Empty />} />
             </Routes>
         </Router>
---
+++ voca/src/component/CreateDay.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const CreateDay = () => {
+    const [days, setDays] = useState([]);
+    const navigate = useNavigate();
+    const fetchDay = async () => {
+        const res = await fetch('http://localhost:3001/days');
+        const json = await res.json();
+        setDays(json);
+    };
+    const addDay = () => {
+        fetch(`http://localhost:3001/days/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                day: days.length + 1,
+            }),
+        }).then((res) => {
+            if (res.ok) {
+                alert('생성이 완료 되었습니다.');
+                navigate('/');
+            }
+        });
+    };
+    useEffect(() => {
+        fetchDay();
+    }, []);
+    return (
+        <div>
+            <h3>현재 일수 : {days.length}일</h3>
+            <button onClick={addDay}>Day 추가</button>
+        </div>
+    );
+};
+
+export default CreateDay;
